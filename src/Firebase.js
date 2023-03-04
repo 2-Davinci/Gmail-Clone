74e5945ec4d2e2ef4addc725b78480d91bdaef95
2-Davinci <116849278+2-Davinci@users.noreply.github.com>
2023-03-04 15:46:50 +0000

    initialize

---
+++ src/Firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyBgT-eo1u9mFjmau1U7niMrgEuDtjaBl8k",
+  authDomain: "clone-1452.firebaseapp.com",
+  projectId: "clone-1452",
+  storageBucket: "clone-1452.appspot.com",
+  messagingSenderId: "992005193837",
+  appId: "1:992005193837:web:097246542913a26d407f64",
+  measurementId: "G-7FBZ5P360B",
+};
+const firebaseapp = fir;
